544b6d2ba82891cfc85985c4c36111802922da5f
Vu Anh Khoa <128579628+vakhoa4875@users.noreply.github.com>
2025-03-17 15:31:02 +0700

    initialize React app with TypeScript, Vite, and Tailwind CSS setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'primereact/button';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <Button label='Look at me!' icon='pi pi-eye'></Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import "primeicons/primeicons.css"; // Icons
+import "primereact/resources/primereact.min.css"; // Core CSS
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
